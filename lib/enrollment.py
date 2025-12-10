953f751c9b435d5d09998790245ef475b734a874
TONNY <blare9.70@gmail.com>
2025-12-10 21:19:44 +0300

    ATP

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -15,6 +15,10 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    # AGGREGATE METHOD
+    def course_count(self):
+        return len(self._enrollments)
+
 class Course:
     def __init__(self, title):
 
@@ -29,6 +33,11 @@ class Course:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+
+    # AGGREGATE METHOD
+    def student_count(self):
+        return len(self._enrollments)
+
 
 
 class Enrollment:
@@ -45,3 +54,12 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    # AGGREGATE METHOD (class level)
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
